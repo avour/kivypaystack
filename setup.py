4878fd533a820834b496f587c351f035123c2e87
Edward Popoola <edwardpopoola@gmail.com>
2016-03-02 18:07:04 +0100

    Minor format changes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='pypaystack',
-      version='0.1',
+      version='0.11',
       description='Python wrapper for Paystack API',
       url='https://github.com/edwardpopoola/pypaystack',
       author='Edward Popoola',
       author_email='edwardpopoola@gmail.com',
       license='MIT',
-      install_requires=['requests'],
+      install_requires=['requests', 'json'],
       packages=['pypaystack'],
       zip_safe=False)
 
